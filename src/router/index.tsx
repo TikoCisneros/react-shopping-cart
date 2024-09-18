9cea6cb229dc06a72ac9f7b35c6959832452968e
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-09-18 11:44:14 -0500

    feat: add project main components structure

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import Layout from '@/layouts/Layout';
+import Home from '@/pages/Home';
+import Details from '@/pages/Details';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/:productId" element={<Details />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
